5c20eb2bd5f05d1373f2040910c5244c6747a632
Nurmuhammad-Ikromov <nurmuhammadikromov58@gmail.com>
2026-02-16 19:34:52 +0500

    add url

--- src/server.js
+++ src/server.js
@@ -13,7 +13,8 @@ const allowedOrigins = [
   "http://localhost:5500",
   "http://127.0.0.1:3000",
   "http://localhost:3000",
-  "https://nafisa-self.vercel.app"
+  "https://nafisa-self.vercel.app",
+  "https://location-admin.vercel.app"
 ];
 
 const corsOptions = {
